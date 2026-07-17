7552ea921e636c48e69c87864c25516950ecc56e
Fabian Graßl <mail@fabiangrassl.de>
2026-07-17 15:47:07 +0200

    fix(cli): require_once vendor/autoload in OpenAPI bin scripts

--- bin/openapi-check.php
+++ bin/openapi-check.php
@@ -2,6 +2,6 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 exit(\Spora\Console\Commands\OpenApiGenerateCommand::checkAgainstFile($argv[1] ?? 'openapi.json'));
